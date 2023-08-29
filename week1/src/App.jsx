9e203154a76edcc0c81c666758e7e456b1994be2
Tina <tina.winnn@gmail.com>
2023-08-29 11:21:14 -0400

    moved files to week1 folder

---
+++ week1/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import CommentPage from './pages/CommentPage'
+
+function App() {
+
+  return (
+    <>
+    <CommentPage />
+    </>
+  )
+}
+
+export default App
