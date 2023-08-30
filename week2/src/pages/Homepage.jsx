942b6d516a73ac98a3d1c86deb60dfd006461fe8
Tina <tina.winnn@gmail.com>
2023-08-29 20:51:13 -0400

    data file created and homepage styled with bc

---
+++ week2/src/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Homepage.css'
+import Header from '../components/Header/Header'
+
+function Homepage() {
+  return (
+    <div>
+    <Header />
+    </div>
+  )
+}
+
+export default Homepage
