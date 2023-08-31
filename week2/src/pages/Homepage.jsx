cde1c07c9c3c4867a9042bd7e850d53e4cf4c601
Tina <tina.winnn@gmail.com>
2023-08-31 17:47:44 -0400

    suggest css and some homepage css

--- week2/src/pages/Homepage.jsx
+++ week2/src/pages/Homepage.jsx
@@ -4,29 +4,35 @@ import Header from '../components/Header/Header'
 import Follow from '../components/Follow/Follow'
 import Stories from '../components/Stories/Stories'
 import next from '../assets/posts/next.png'
+import Suggest from '../components/Suggest/Suggest'
 
 function Homepage({userData}) {
   return (
     <div>
     <Header />
     <section className='main-container'>
-      <div className='stories-container'>
-        {
-          userData[1].users.map((item, index) => <Stories item={item} index={index} key={item.key}/>)
-        }
-        <img className='stories-nextbtn' src={next} alt='next-btn'/>
+      <div className='left-container'>
+          <div className='stories-container'>
+          {
+            userData[1].users.map((item, index) => <Stories item={item} index={index} key={item.key}/>)
+          }
+          <img className='stories-nextbtn' src={next} alt='next-btn'/>
+          </div>
+          <div className='suggest-container'>
+          <Suggest item={userData[2].users}/>
+          </div>
       </div>
       <div className='follow-right'>
-      <Follow item={userData[0].users[0]} style={{backgroundColor: '#F5F5F5'}}   />
-      <div className='right-sug'>
-      <p>Suggestions for you</p>
-      <p>See All</p>
-      </div>
-      <Follow item={userData[0].users[1]}/>
-      <Follow item={userData[0].users[2]}/>
-      <Follow item={userData[0].users[3]}/>
-      <Follow item={userData[0].users[4]}/>
-      <Follow item={userData[0].users[5]}/>
+          <Follow item={userData[0].users[0]} style={{backgroundColor: '#F5F5F5'}}   />
+          <div className='right-sug'>
+          <p>Suggestions for you</p>
+          <p>See All</p>
+          </div>
+          <Follow item={userData[0].users[1]}/>
+          <Follow item={userData[0].users[2]}/>
+          <Follow item={userData[0].users[3]}/>
+          <Follow item={userData[0].users[4]}/>
+          <Follow item={userData[0].users[5]}/>
       </div>
     </section>
     </div>
