6e4b778d610ef7447b10b7ff9054976c15678bf9
Tina <tina.winnn@gmail.com>
2023-08-31 21:40:33 -0400

    added other posts

--- week2/src/pages/Homepage.jsx
+++ week2/src/pages/Homepage.jsx
@@ -12,15 +12,19 @@ function Homepage({userData}) {
     <div>
     <Header />
     <section className='main-container'>
-      <div className='left-container'>
+      <div>
           <div className='stories-container'>
           {
             userData[1].users.map((item, index) => <Stories item={item} index={index} key={item.key}/>)
           }
           <img className='stories-nextbtn' src={next} alt='next-btn'/>
           </div>
+          <div className='all-posts'>
           <Posts item={userData[3].users[0]}/>
+          <Posts item={userData[3].users[1]} style={{display: 'none'}}/>
           <Suggest item={userData[2].users}/>
+          <Posts item={userData[3].users[2]} style={{display: 'none'}}/>
+          </div>
       </div>
       <div className='follow-right'>
           <Follow item={userData[0].users[0]} style={{backgroundColor: '#F5F5F5'}}   />
