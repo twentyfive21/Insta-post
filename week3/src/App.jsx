5d846f96ad20cc0cbeef7bb91d2fb0b2538142d1
twentyfive21 <tina.winnn@gmail.com>
2023-09-13 19:51:28 -0400

    cleanup function for window resize

--- week3/src/App.jsx
+++ week3/src/App.jsx
@@ -4,29 +4,35 @@ import './App.css'
 function App() {
 
   const screen = useRef();
+  const [resize, setResize] = useState(true)
   const [size, setSize] = useState({
     width: 0,
     height: 0
   })
 
-  useEffect(()=>{
+  const handleResize = () => {
     setSize({
       width: screen.current.clientWidth,
       height: screen.current.clientHeight
     })
-  },[])
+  }
 
   useEffect(()=>{
-    window.addEventListener('resize', ()=> setSize({
-     width: screen.current.clientWidth,
-     height: screen.current.clientHeight
-   })) 
-   },[])
+    if(resize){
+      window.addEventListener('resize', handleResize) 
+    }
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+   },[resize])
 
 
   return (
     <div ref={screen}>
      <h1>Window Screen Size</h1>
+     <button onClick={()=>setResize(!resize)}>
+    {resize ? 'Turn resize off' : 'Turn resize on'}
+    </button>
      <p>Width : {size.width}px</p>
      <p>Height : {size.height}px</p>
     </div>
